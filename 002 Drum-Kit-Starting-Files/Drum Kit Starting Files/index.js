04e23ebe8742c406aea4a66176d054af7a98b18b
Mohamed Afraar <afraararm@gmail.com>
2024-10-02 23:51:38 +0530

    Refactor Drum Kit JavaScript: Add switch statement for button click event

--- 002 Drum-Kit-Starting-Files/Drum Kit Starting Files/index.js
+++ 002 Drum-Kit-Starting-Files/Drum Kit Starting Files/index.js
@@ -4,7 +4,16 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
   document.querySelectorAll("button")[i].addEventListener("click", function () {
 
-    this.style.color = "white";
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+
+    // this.style.color = "white";
     // var audio = new Audio('./sounds/tom-1.mp3');
     // audio.play();
   });
