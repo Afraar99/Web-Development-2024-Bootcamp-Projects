7beb5c170fc2e0015255e1430bb77074b6ef88dd
Mohamed Afraar <afraararm@gmail.com>
2024-10-03 11:28:53 +0530

    finished drumkit

--- 002 Drum-Kit-Starting-Files/Drum Kit Starting Files/index.js
+++ 002 Drum-Kit-Starting-Files/Drum Kit Starting Files/index.js
@@ -5,12 +5,14 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
 //detecting keyboard press
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -55,4 +57,13 @@ function makeSound(key) {
 
       break;
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
