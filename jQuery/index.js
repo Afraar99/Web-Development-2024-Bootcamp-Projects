5fabe21d0a436ce3542a0e6c4be56e26d1dd4bf8
Mohamed Afraar <afraararm@gmail.com>
2024-10-04 11:37:33 +0530

    Refactor jQuery files: Update HTML, JavaScript, and CSS

---
+++ jQuery/index.js
@@ -0,0 +1,5 @@
+// $("h1").css("color","green");
+$("button").html("<em>Hey</em>");
+$("a").attr("href","https://www.yahoo.com");
+$("h1").addClass("big-title margin-50");
+$("h1").text("Hello World");
