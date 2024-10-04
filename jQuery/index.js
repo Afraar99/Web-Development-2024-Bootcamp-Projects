fb0f9548b6a68d7d760680608674f7f0993a6da6
Mohamed Afraar <afraararm@gmail.com>
2024-10-04 16:05:07 +0530

    Refactor jQuery files: Update HTML, JavaScript, and CSS

--- jQuery/index.js
+++ jQuery/index.js
@@ -3,3 +3,16 @@ $("button").html("<em>Hey</em>");
 $("a").attr("href","https://www.yahoo.com");
 $("h1").addClass("big-title margin-50");
 $("h1").text("Hello World");
+
+//event listeners
+$("button").click(function(){
+    $("h1").css("color","red");
+})
+
+$(document).keypress(function(event){
+    $("h1").text(event.key);
+})
+
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "purple");
+})
